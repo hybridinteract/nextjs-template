cf04faab0a8c050a7151e93aa1fcea0082de9947
avi9611 <avinashpoojary651@gmail.com>
2026-05-04 14:08:21 +0530

    refactor: remove useTrackedRouter and associated router integration logic

--- src/lib/loading/index.ts
+++ src/lib/loading/index.ts
@@ -1,4 +1,3 @@
 export type { BlockingLoadSource, LoadingEntry } from "./types";
 export { useBlockingLoadingStore, startBlockingLoad } from "./store";
 export { useBlockingMutation } from "./mutation";
-export { useTrackedRouter } from "./router";
